19e08877bc30d2b05c346b725bab78c73851f561
runminty <zengzhenhuang@gmail.com>
2019-04-12 10:35:48 +1200

    add component structure

---
+++ client/components/Sheep.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+class Sheep extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+
+
+// sound event listener here 
+
+
+render(){
+
+  }
+}
+
+
+export default Sheep
